1ed7f6364b0d065ccb795cc96b5ee820c69795f9
EDUARDO MOREIRA FELIPPI <04013277018@senacrs.edu.br>
2026-06-23 17:23:56 -0300

    continuação em API

--- api/viajei_api/app.py
+++ api/viajei_api/app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from viajei_api.schemas import Message
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', response_model=Message)
 def read_root():
-    return ["( ͡° ͜ʖ ͡°)"]
+    return {"message": "Salve tropa"}
+
+@app.get('/hello', response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+        <head>
+            <title> Hello World! </title>
+        </head>
+        <body>
+            <h1>Olá mundo!</h1>
+        </body>
+    </html>"""
