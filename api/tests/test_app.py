9a5d1096830217295ed9f498f23b0d6c2abbe404
EDUARDO MOREIRA FELIPPI <04013277018@senacrs.edu.br>
2026-06-30 17:25:17 -0300

    Alembic

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -43,3 +43,30 @@ def test_delete_user(client):
 
     response.status_code == HTTPStatus.OK
     response.json() == {"message": "User deleted"}
+
+
+def test_delete_notfound(client):
+
+    response = client.delete("/users/0")
+
+    response.status_code == HTTPStatus.NOT_FOUND
+    response.json() == {"message": "User not found"}
+
+
+def test_get_notfound(client):
+
+    response = client.get("/users/0")
+
+    response.status_code == HTTPStatus.NOT_FOUND
+    response.json() == {"detail": "User not found"}
+
+
+def test_get_200(client):
+
+    response = client.get("/users/1")
+
+    response.status_code == HTTPStatus.OK
+    response.json() == {
+        "email": "jorge@example.com",
+        "id": 1,
+    }
